7f00bf7072b75d1592a60cb2f776b2a01bc5d6ce
Tannenhe <102766352+Tannenhe@users.noreply.github.com>
2022-04-01 17:13:31 -0700

    Update python_numpy1.py

--- PythonAndNumpy1/python_numpy1.py
+++ PythonAndNumpy1/python_numpy1.py
@@ -3,13 +3,6 @@
 """Python Essentials: Introduction to Python.
 Ehman Tannenholz
 MTH 420
-4-1-22"""
-
-# Problem 1
-
-pi = 3.14159
-
-def volume(r):
-    return (4 / 3)*pi*(r^3)
-
-volume(10)
+4-1-22
+"""
+print("Hello, world!")
