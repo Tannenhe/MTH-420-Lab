4e3180028b2d171c937fce8dc8e24289c7fe6fed
Tannenhe <102766352+Tannenhe@users.noreply.github.com>
2022-04-12 14:15:52 -0700

    Update standard_library.py

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -1,8 +1,8 @@
 # standard_library.py
 """Python Essentials: The Standard Library.
-<Name>
-<Class>
-<Date>
+<Ehman Tannenholz>
+<MTH 420>
+<4/12/22>
 """
 
 
@@ -11,7 +11,7 @@ def prob1(L):
     """Return the minimum, maximum, and average of the entries of L
     (in that order).
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    return print(min(L), max(L), st.mean(L), sep=", ")
 
 
 # Problem 2
@@ -19,8 +19,36 @@ def prob2():
     """Determine which Python objects are mutable and which are immutable.
     Test numbers, strings, lists, tuples, and sets. Print your results.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    
+int_1 = 1
+int_2 = int_1
+int_2 = 2
+int_2 == int_1
+#int is immutable
 
+str_1 = 'apples'
+str_2 = str_1
+str_2 = 'bananas'
+str_2 == str_1
+#str is immutable
+
+list_1 = [1,2,3]
+list_2 = list_1
+list_2[1] = 4
+list_2 == list_1
+#list is mutable
+
+tuple_1 = (1,2,3)
+tuple_2 = tuple_1
+tuple_2 += (1,)
+tuple_2 == tuple_1
+#tuple is immutable
+
+set_1 = {1,2,3}
+set_2 = set_1
+set_2.remove(1)
+set_2 == set_1
+#set is mutable
 
 # Problem 3
 def hypot(a, b):
