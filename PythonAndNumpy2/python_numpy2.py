8d9112f672b00cd80ac75bf2aae1f6dbb018c8d4
Tannenhe <102766352+Tannenhe@users.noreply.github.com>
2022-04-22 17:54:43 -0700

    Update python_numpy2.py
    problems 1-6, grid.npy missing from directory for problem 8.

--- PythonAndNumpy2/python_numpy2.py
+++ PythonAndNumpy2/python_numpy2.py
@@ -1,36 +1,57 @@
 # python_intro.py
 """Python Essentials: Introduction to Python.
-<Name>
-<Class>
-<Date>
+<Ehman Tannenholz>
+<MTH 420>
+<4/22/22>
 """
 
 #Problem 1
 def isolate(a, b, c, d, e):
+    
+    return print(a,b,c,sep='     ',end=' '), print(d,e,sep=' ')
 
-    raise NotImplementedError("Problem 1 Incomplete")
+
+
 
 #Problem 2
 def first_half(string):
-
-    raise NotImplementedError("Problem 2 Incomplete")
+    fhalf = int(0.5*len(string))
+    return string[:fhalf]
 
 
 def backward(first_string):
+    back = ""
+    for i in range(len(first_string)+1):
+        back+=first_string[-i]
+    return back
 
-    raise NotImplementedError("Problem 2 Incomplete")
+
+
+
+
 
 #Problem 3
 def list_ops():
+    list = ["bear","ant","cat","dog"]
+    list.append("eagle")
+    list[2] = "fox"
+    list.pop(1)
+    list.sort(reverse=True)
+    list[list.index("eagle")] = "hawk"
+    hunt = ["hunter"]
+    list = list + hunt
+    return list
 
-    raise NotImplementedError("Problem 3 Incomplete")
+
+
 
 #Problem 4
 def alt_harmonic(n):
     """Return the partial sum of the first n terms of the alternating
     harmonic series. Use this function to approximate ln(2).
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    harm = [(-1)**(i+1)/i for i in range(1,n+1)]
+    return sum(harm)
 
 
 
@@ -43,7 +64,11 @@ def prob5(A):
         >>> prob4(A)
         array([0, 0, 3])
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    import numpy as np
+    copy = A
+    mask = A < 0
+    copy[mask] = 0
+    return copy
 
 def prob6():
     """Define the matrices A, B, and C as arrays. Return the block matrix
@@ -53,7 +78,24 @@ def prob6():
     where I is the 3x3 identity matrix and each 0 is a matrix of all zeros
     of the appropriate size.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    import numpy as np
+    A = np.arange(6).reshape(3,2)
+    A = A.T
+    B = np.zeros(9)
+    maskB = [0,3,4,6,7,8]
+    B[maskB]=3
+    B = B.reshape((3,3))
+    C = np.zeros(9)
+    maskC = [0,4,8]
+    C[maskC]=-2
+    C = C.reshape((3,3))
+    
+    one = np.hstack((np.zeros((3,3)),A.T,np.eye(3)))
+    two = np.hstack((A,np.zeros((2,2)),np.zeros((2,3))))
+    three = np.hstack((B,np.zeros((3,2)),C))
+    stack = np.vstack((one,two,three))
+    
+    return print(stack)
 
 def prob7(A):
     """Divide each row of 'A' by the row sum and return the resulting array.
@@ -65,7 +107,7 @@ def prob7(A):
                [ 0.        ,  1.        ,  0.        ],
                [ 0.33333333,  0.33333333,  0.33333333]])
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    
 
 
 def prob8():
@@ -74,5 +116,4 @@ def prob8():
     diagonally) in the grid.
     """
     raise NotImplementedError("Problem 8 Incomplete")
-
 
